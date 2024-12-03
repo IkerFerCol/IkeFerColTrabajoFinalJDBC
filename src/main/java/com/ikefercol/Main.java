53a1cfe7c517e3898b28b442ca097dcce31ff790
IkerFerCol <ikefercol@alu.edu.gva.es>
2024-12-03 18:41:45 +0100

    3/12/24

--- src/main/java/com/ikefercol/Main.java
+++ src/main/java/com/ikefercol/Main.java
@@ -1,102 +1,59 @@
 package com.ikefercol;
 
-import java.io.*;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql:src/main/resources/sqlite.db";
+        String url = "jdbc:sqlite:src/main/resources/sqlite.db";
+        String sql = "src/main/resources/script.sql";
 
-        try (Connection conn = DriverManager.getConnection(url)){
-            if (conn != null) {
-                System.out.println("¡Conectado a la Base de Datos!");
+        try (Connection connection = DriverManager.getConnection(url)){
+            Statement statement = connection.createStatement();
+            String archivoleidosql = new String(Files.readAllBytes(Paths.get(sql)));
+            statement.executeUpdate(archivoleidosql);
+            System.out.println("--| Base de datos creada con sus datos insertados |--");
 
-                String archivoSQL = new String(Files.readAllBytes(Paths.get("src/main/resources/script.sql")));
-
-                try (Statement stmt = conn.createStatement()) {
-                    stmt.execute(archivoSQL);
-                    System.out.println("Tablas Creadas con sus Datos insertados.");
-                }
-
-                String consulta1 = "SELECT nombre FROM EQUIPO WHERE liga_id = 1";
-                try (Statement stmt = conn.createStatement()) {
-                    ResultSet rs = stmt.executeQuery(consulta1);
-                    while (rs.next()) {
-                        String nombre = rs.getString("nombre");
-                        System.out.println("Equipo: " + nombre);
-                    }
-                }
-
-                String consulta2 = "SELECT nombre FROM JUGADOR WHERE equipo_id = 1";
-
-                try (Statement stmt = conn.createStatement();
-                     ResultSet rs2 = stmt.executeQuery(consulta2)) {
-                    System.out.println("Jugadores en Real Madrid:");
-                    while (rs2.next()) {
-                        System.out.println("Jugador: " + rs2.getString("nombre"));
-                    }
-                }
-
-                String consulta3 = "SELECT J.nombre FROM JUGADOR J JOIN JUGADOR_LIGA JL ON J.id = JL.jugador_id WHERE JL.liga_id = ?";
-                try (PreparedStatement pstmt = conn.prepareStatement(consulta3)) {
-                    pstmt.setInt(1, 1); // Liga ID
-                    try (ResultSet rs3 = pstmt.executeQuery()) {
-                        System.out.println("Jugadores en LaLiga:");
-                        while (rs3.next()) {
-                            System.out.println("Jugador: " + rs3.getString("nombre"));
-                        }
-                    }
-                }
-
-                String consulta4 = "SELECT E.nombre AS equipo, EST.nombre AS estadio FROM EQUIPO E JOIN ESTADIO EST ON E.id = EST.equipo_id WHERE E.liga_id = ?";
-                try (PreparedStatement pstmt = conn.prepareStatement(consulta4)) {
-                    pstmt.setInt(1, 1); // Liga ID
-                    try (ResultSet rs4 = pstmt.executeQuery()) {
-                        System.out.println("Estadios en LaLiga:");
-                        while (rs4.next()) {
-                            System.out.println("Equipo: " + rs4.getString("equipo") + ", Estadio: " + rs4.getString("estadio"));
-                        }
-                    }
-                }
+            ResultSet resultSet = statement.executeQuery("SELECT E.nombre AS equipo, L.nombre AS liga FROM EQUIPO E JOIN LIGA L ON E.liga_id = L.id");
+            System.out.println("** Equipos y sus ligas: ");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("equipo") + " " + resultSet.getString("liga"));
             }
+            System.out.println();
+
+            ResultSet resultSet2 = statement.executeQuery("SELECT J.nombre AS jugador, E.nombre AS equipo FROM JUGADOR J JOIN EQUIPO E ON J.equipo_id = E.id");
+            System.out.println("** Jugadores y sus equipos: ");
+            while (resultSet2.next()) {
+                System.out.println(resultSet2.getString("jugador") + " " + resultSet2.getString("equipo"));
+            }
+            System.out.println();
+
+            PreparedStatement preparedStatement = connection.prepareStatement(" SELECT J.nombre FROM JUGADOR J JOIN EQUIPO E ON J.equipo_id = E.id WHERE E.nombre = ?");
+            preparedStatement.setString(1, "Real Madrid");
+            ResultSet resultSet3 = preparedStatement.executeQuery();
+            System.out.println("** Jugadores del Real Madrid: ");
+            while (resultSet3.next()) {
+                System.out.println(resultSet3.getString("nombre"));
+            }
+            System.out.println();
+
+            PreparedStatement preparedStatement2 = connection.prepareStatement("SELECT ES.nombre AS estadio, ES.capacidad FROM ESTADIO ES WHERE ES.capacidad > ?");
+            preparedStatement2.setInt(1, 90000);
+            ResultSet resultSet4 = preparedStatement2.executeQuery();
+            System.out.println("** Estadios con capacidad mayor a 90000:");
+            while (resultSet4.next()) {
+                String estadio = resultSet4.getString("estadio");
+                int capacidad = resultSet4.getInt("capacidad");
+                System.out.println(estadio + " (" + capacidad + ")");
+            }
+            System.out.println();
+
 
         } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
-
-
-//        Connection connection = null;
-//        PreparedStatement preparedStatement = null;
-//        Statement statement = null;
-//        ResultSet resultSet = null;
-//
-//        try {
-//            String url = "jdbc:sqlite:src/main/resources/sqlite.db";
-//            connection = DriverManager.getConnection(url);
-//            System.out.println("¡Conectado a la base de datos!");
-//
-//            statement = connection.createStatement();
-//
-//            String c1 = "SELECT J.nombre AS jugador, E.nombre AS equipo FROM JUGADOR J JOIN EQUIPO E ON J.equipo_id = E.id";
-//            resultSet = statement.executeQuery(c1);
-//            while (resultSet.next()) {
-//                System.out.println(resultSet.getString("jugador") + " " + resultSet.getString("equipo"));
-//            }
-//
-//
-//            connection.close();
-//            preparedStatement.close();
-//            statement.close();
-//            resultSet.close();
-//
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-
-
-
 
     }
 }
--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,92 +1,82 @@
---CREATE DATABASE IF NOT EXISTS equiposfutbol
-CREATE TABLE IF NOT EXISTS LIGA
-(
-    id     INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre VARCHAR(50) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS EQUIPO
-(
-    id            INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre        VARCHAR(50)  NOT NULL,
-    lugar         VARCHAR(50)  NOT NULL,
-    mejor_jugador VARCHAR(50)  NOT NULL,
-    imagen        VARCHAR(200) NOT NULL,
-    liga_id       INT,
+-- Crear la tabla LIGA
+CREATE TABLE IF NOT EXISTS LIGA (
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    nombre VARCHAR(50) NOT NULL
+    );
+
+-- Crear la tabla EQUIPO
+CREATE TABLE IF NOT EXISTS EQUIPO (
+                                      id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                      nombre VARCHAR(50) NOT NULL,
+    lugar VARCHAR(50) NOT NULL,
+    mejor_jugador VARCHAR(50) NOT NULL,
+    imagen VARCHAR(200) NOT NULL,
+    liga_id INT,
     FOREIGN KEY (liga_id) REFERENCES LIGA (id)
-);
+    );
 
-
-CREATE TABLE IF NOT EXISTS ESTADIO
-(
-    id        INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre    VARCHAR(50) NOT NULL,
-    capacidad INT         NOT NULL,
+-- Crear la tabla ESTADIO
+CREATE TABLE IF NOT EXISTS ESTADIO (
+                                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                       nombre VARCHAR(50) NOT NULL,
+    capacidad INT NOT NULL,
     equipo_id INT UNIQUE,
     FOREIGN KEY (equipo_id) REFERENCES EQUIPO (id)
-);
+    );
 
-
-CREATE TABLE IF NOT EXISTS JUGADOR
-(
-    id        INTEGER PRIMARY KEY AUTOINCREMENT,
-    nombre    VARCHAR(50) NOT NULL,
+-- Crear la tabla JUGADOR
+CREATE TABLE IF NOT EXISTS JUGADOR (
+                                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                       nombre VARCHAR(50) NOT NULL,
     equipo_id INT,
     FOREIGN KEY (equipo_id) REFERENCES EQUIPO (id)
-);
+    );
 
-
-CREATE TABLE IF NOT EXISTS JUGADOR_LIGA
-(
-    jugador_id INT,
-    liga_id    INT,
-    PRIMARY KEY (jugador_id, liga_id),
+-- Crear la tabla JUGADOR_LIGA
+CREATE TABLE IF NOT EXISTS JUGADOR_LIGA (
+                                            jugador_id INT,
+                                            liga_id INT,
+                                            PRIMARY KEY (jugador_id, liga_id),
     FOREIGN KEY (jugador_id) REFERENCES JUGADOR (id),
     FOREIGN KEY (liga_id) REFERENCES LIGA (id)
-);
+    );
 
-
+-- Insertar datos en la tabla LIGA
 INSERT INTO LIGA (nombre)
-VALUES ('LaLiga'),
-       ('PremierLeague');
+VALUES ('LaLiga'), ('PremierLeague');
 
-
+-- Insertar datos en la tabla EQUIPO
 INSERT INTO EQUIPO (nombre, lugar, mejor_jugador, imagen, liga_id)
-VALUES ('Real_Madrid', 'Madrid', 'Vinícius Júnior',
-        'https://assets.laliga.com/squad/2024/t186/p246333/256x278/p246333_t186_2024_1_001_000.png', 1),
-       ('FC_Barcelona', 'Barcelona', 'Robert Lewandowski',
-        'https://assets.laliga.com/squad/2024/t178/p56764/256x278/p56764_t178_2024_1_001_000.png', 1),
-       ('Villarreal_CF', 'Vila-real', 'Alex Baena',
-        'https://assets.laliga.com/squad/2024/t449/p248501/256x278/p248501_t449_2024_1_001_000.png', 1),
-       ('Real_Betis_Balompie', 'Sevilla', 'Giovani Lo Celso',
-        'https://assets.laliga.com/squad/2024/t185/p200826/2048x2048/p200826_t185_2024_1_002_000.jpg', 1),
-       ('Valencia_CF', 'Valencia', 'Gaya',
-        'https://assets.laliga.com/squad/2024/t191/p132105/2048x2048/p132105_t191_2024_1_002_000.jpg', 1),
-       ('Manchester_City', 'Manchester', 'Erling Haaland',
-        'https://resources.premierleague.com/premierleague/photos/players/250x250/p223094.png', 2),
-       ('Arsenal', 'Londres', 'Bukayo Saka',
-        'https://resources.premierleague.com/premierleague/photos/players/250x250/p223340.png', 2),
-       ('Manchester_United', 'Manchester', 'Bruno Fernandes',
-        'https://resources.premierleague.com/premierleague/photos/players/250x250/p141746.png', 2),
-       ('Liverpool_FC', 'Liverpool', 'Mohamed Salah',
-        'https://resources.premierleague.com/premierleague/photos/players/250x250/p118748.png', 2),
-       ('Chelsea_FC', 'Londres', 'Cole Palmer',
-        'https://resources.premierleague.com/premierleague/photos/players/250x250/p244851.png', 2);
+VALUES
+    ('Real Madrid', 'Madrid', 'Vinícius Júnior', 'https://assets.laliga.com/squad/2024/t186/p246333/256x278/p246333_t186_2024_1_001_000.png', 1),
+    ('FC Barcelona', 'Barcelona', 'Robert Lewandowski', 'https://assets.laliga.com/squad/2024/t178/p56764/256x278/p56764_t178_2024_1_001_000.png', 1),
+    ('Villarreal CF', 'Vila-real', 'Alex Baena', 'https://assets.laliga.com/squad/2024/t449/p248501/256x278/p248501_t449_2024_1_001_000.png', 1),
+    ('Real Betis Balompié', 'Sevilla', 'Giovani Lo Celso', 'https://assets.laliga.com/squad/2024/t185/p200826/2048x2048/p200826_t185_2024_1_002_000.jpg', 1),
+    ('Valencia CF', 'Valencia', 'Gaya', 'https://assets.laliga.com/squad/2024/t191/p132105/2048x2048/p132105_t191_2024_1_002_000.jpg', 1),
+    ('Manchester City', 'Manchester', 'Erling Haaland', 'https://resources.premierleague.com/premierleague/photos/players/250x250/p223094.png', 2),
+    ('Arsenal', 'Londres', 'Bukayo Saka', 'https://resources.premierleague.com/premierleague/photos/players/250x250/p223340.png', 2),
+    ('Manchester United', 'Manchester', 'Bruno Fernandes', 'https://resources.premierleague.com/premierleague/photos/players/250x250/p141746.png', 2),
+    ('Liverpool FC', 'Liverpool', 'Mohamed Salah', 'https://resources.premierleague.com/premierleague/photos/players/250x250/p118748.png', 2),
+    ('Chelsea FC', 'Londres', 'Cole Palmer', 'https://resources.premierleague.com/premierleague/photos/players/250x250/p244851.png', 2);
 
-
+-- Insertar datos en la tabla ESTADIO
 INSERT INTO ESTADIO (nombre, capacidad, equipo_id)
-VALUES ('Santiago Bernabéu', 81044, 1),
-       ('Camp Nou', 99354, 2),
-       ('Estadio de la Cerámica', 23000, 3),
-       ('Estadio Benito Villamarín', 60000, 4),
-       ('Estadio de Mestalla', 55000, 5),
-       ('Etihad Stadium', 55017, 6),
-       ('Emirates Stadium', 60000, 7),
-       ('Old Trafford', 74879, 8),
-       ('Anfield', 54074, 9),
-       ('Stamford Bridge', 40848, 10);
+VALUES
+    ('Santiago Bernabéu', 81044, 1),
+    ('Camp Nou', 99354, 2),
+    ('Estadio de la Cerámica',  23000, 3),
+    ('Estadio Benito Villamarín', 60000, 4),
+    ('Estadio de Mestalla', 55000, 5),
+    ('Etihad Stadium', 55017, 6),
+    ('Emirates Stadium', 60000, 7),
+    ('Old Trafford', 74879, 8),
+    ('Anfield', 54074, 9),
+    ('Stamford Bridge', 40848, 10);
 
-    ('Vinicius Jr', 1)
-    ,
+-- Insertar datos en la tabla JUGADOR
+INSERT INTO JUGADOR (nombre, equipo_id)
+VALUES
+    ('Vinícius Júnior', 1),
     ('Robert Lewandowski', 2),
     ('Alex Baena', 3),
     ('Giovani Lo Celso', 4),
@@ -96,30 +86,3 @@ VALUES ('Santiago Bernabéu', 81044, 1),
     ('Bruno Fernandes', 8),
     ('Mohamed Salah', 9),
     ('Cole Palmer', 10);
-
-INSERT INTO JUGADOR (nombre, equipo_id)
-VALUES ('Vinicius Jr', 1),
-       ('Robert Lewandowski', 2),
-       ('Alex Baena', 3),
-       ('Giovani Lo Celso', 4),
-       ('Gaya', 5),
-       ('Erling Haaland', 6),
-       ('Bukayo Saka', 7),
-       ('Bruno Fernandes', 8),
-       ('Mohamed Salah', 9),
-       ('Cole Palmer', 10);
-
-
-INSERT INTO JUGADOR_LIGA (jugador_id, liga_id)
-VALUES (1, 1),
-       (2, 1),
-       (3, 1),
-       (4, 1),
-       (5, 1),
-       (6, 2),
-       (7, 2),
-       (8, 2),
-       (9, 2),
-       (10, 2);
-
-
